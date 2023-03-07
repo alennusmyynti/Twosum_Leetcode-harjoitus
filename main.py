2c519bdfe51989dc99a41554acb3b897e97eae6c
alennusmyynti <63056367+alennusmyynti@users.noreply.github.com>
2023-03-07 17:40:24 +0200

    Update main.py
    Koodin korjaus

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 nums = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9]
 target = 10
 
+printtext = 0
+#indicates whether or not the system prints text
 
 
 class Solution:
@@ -21,10 +23,35 @@ class Solution:
           if self.nums[x] + self.nums[y] == target:
 
             print([x, y])
-            
-    
+            if printtext == 1:
+              print(self.nums[x], "+", self.nums[y], "=", self.target)
+              self.total += 1
 
+              print("")
+
+    if printtext == 1:
+      print("")
+      print("Total:", self.total)
+
+
+if printtext == 1:
+  print("Solutions:")
+  print("")
 
 ratkaisu = Solution()
 ratkaisu.twoSum(nums, target)
+if printtext == 1:
+  print("")
+  print("------------------------------------------------")
+  print("")
+  print("Used Values:")
+  for z in range(len(nums)):
 
+    print(z, " - ", nums[z])
+
+  print("")
+  print("Two-Sum Target:")
+  print(ratkaisu.target)
+
+  print("")
+  print("Credit: eelis")
